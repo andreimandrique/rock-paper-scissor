8592d0959a4b822c097aa2bca5b2cd5e2c39f237
Kiyotaka327 <andreimandrique3@gmail.com>
2024-03-11 00:03:02 +0800

    add functionality
    I add functionality to my Rock Paper Scissor

--- index.js
+++ index.js
@@ -19,20 +19,84 @@ function getComputerChoice(){
     return computer;
 }
 
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissor");
+
+const roundDisplay = document.getElementById("roundDisplay");
+const scoreDisplay = document.getElementById("scoreDisplay");
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const statusDisplay = document.getElementById("statusDisplay");
+const resetDisplay = document.getElementById("resetDisplay");
+
+let score = 0;
+let round = 0;
+
 function playGame(playerSelection, computerSelection){
 
     let output;
 
-    (playerSelection=='rock' && computerSelection == 'scissor')?
-    output='You win':output='You lose';
-    (playerSelection=='paper' && computerSelection == 'rock')?
-    output='You win':output='You lose';
-    (playerSelection=='scissor' && computerSelection == 'paper')?
-    output='You win':output='You lose';
-    
-    if(playerSelection == computerSelection){
-        output = 'It\'s a draw';
-    }
+    if((playerSelection=='rock'&&computerSelection=='scissor')||
+        (playerSelection=='paper'&&computerSelection=='rock')||
+        (playerSelection=='scissor'&&computerSelection=='paper')){
+            score++;
+            round++;
+            roundDisplay.innerHTML = `Round: ${round}`;
+            scoreDisplay.innerHTML = `Score: ${score}`;
+            statusDisplay.innerText = 'You win';
+        }
+        else if(playerSelection==computerSelection){
+            round++;
+            roundDisplay.innerHTML = `Round: ${round}`;
+            scoreDisplay.innerHTML = `Score: ${score}`;
+            statusDisplay.innerText = 'It\'s a draw';
+        }
+        else{
+            round++;
+            roundDisplay.innerHTML = `Round: ${round}`;
+            scoreDisplay.innerHTML = `Score: ${score}`;
+            statusDisplay.innerText = 'You lose';
+        }
 
     return output;
 }
+
+rock.addEventListener("click", ()=>{
+    let player = 'rock';
+    const computer = getComputerChoice();
+    playGame(player, computer);
+    displayPlayerComputer(player, computer);
+})
+
+paper.addEventListener("click", ()=>{
+    let player = 'paper';
+    const computer = getComputerChoice();
+    playGame(player, computer);
+    displayPlayerComputer(player, computer);
+})
+
+scissor.addEventListener("click", ()=>{
+    let player = 'scissor';
+    const computer = getComputerChoice();
+    playGame(player, computer);
+    displayPlayerComputer(player, computer);
+})
+
+function displayPlayerComputer(playerSelectionDisplay, computerSelectionDisplay){
+    playerDisplay.innerText = `Player: ${playerSelectionDisplay}`;
+    computerDisplay.innerText = `Computer: ${computerSelectionDisplay}`;
+}
+
+resetDisplay.addEventListener("click", ()=>{
+    
+    roundDisplay.innerText = "Round:";
+    scoreDisplay.innerText = "Score:";
+    playerDisplay.innerText = "Player:";
+    computerDisplay.innerText = "Computer:";
+    statusDisplay.innerText = "Default";
+
+    score = 0;
+    round = 0;
+
+})
